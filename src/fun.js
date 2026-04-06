5a4050651ee01f81132f4fc7bcad48864bd2cb46
TrusByte Admin <trustbyte@protonmail.com>
2026-04-06 17:43:41 +0530

    Commit1.0.0.3

--- src/fun.js
+++ src/fun.js
@@ -2,15 +2,17 @@ function formatNumberShort(num) {
     num = parseFloat(num);
 
     if (num >= 1e9) {
-        return (num / 1e9).toFixed(2).replace(/\.00$/, '') + "B";
+        return (num / 1e9).toFixed(2) + "B";
     }
     if (num >= 1e6) {
-        return (num / 1e6).toFixed(2).replace(/\.00$/, '') + "M";
+        return (num / 1e6).toFixed(2) + "M";
     }
     if (num >= 1e3) {
-        return (num / 1e3).toFixed(2).replace(/\.00$/, '') + "K";
+        return (num / 1e3).toFixed(2) + "K";
     }
-    return num.toString();
+
+    // always 2 decimals for small numbers
+    return num.toFixed(2);
 }
 function formatUnits(value, decimals = 18, maxDecimals = 4) {
     value = value.toString();
